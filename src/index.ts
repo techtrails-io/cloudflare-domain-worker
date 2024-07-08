f5fac31421a87d022fe6c3d615872a7ac6813832
Olivier Lance <olivier@techtrails.io>
2024-07-08 20:56:51 +0200

    Remove unused boilerplate code & adapt index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,43 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Router } from 'itty-router';
+// @ts-ignore
+import subscribersIndex from './static/substack-subscribers-index.html';
 
-import handleProxy from './proxy';
-import handleRedirect from './redirect';
-import apiRouter from './router';
+class ElementHandler {
+	element(element: Element) {
+		element.append(
+			'<script defer data-domain="techtrails.io" data-api="https://owl.techtrails.workers.dev/techtrails/owl" src="https://owl.techtrails.workers.dev/techtrails/script.js"></script>',
+			{ html: true }
+		);
+	}
+}
 
-// Export a default object containing event handlers
 export default {
 	// The fetch handler is invoked when this worker receives a HTTP(S) request
 	// and should return a Response (optionally wrapped in a Promise)
 	async fetch(request, env, ctx): Promise<Response> {
-		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
-		const url = new URL(request.url);
+		ctx.passThroughOnException();
 
-		// You can get pretty far with simple logic like if/switch-statements
-		switch (url.pathname) {
-			case '/redirect':
-				return handleRedirect.fetch(request, env, ctx);
+		const router = Router();
 
-			case '/proxy':
-				return handleProxy.fetch(request, env, ctx);
-		}
+		// Substack subscribers iframe page
+		router.get('/substack-subscribers', () => {
+			return new Response(subscribersIndex, {
+				headers: {
+					'Content-Type': 'text/html;charset=UTF-8',
+					'Content-Security-Policy': 'frame-ancestors \'self\' https://speakerine.app https://*.speakerine.app;'
+				}
+			});
+		});
 
-		if (url.pathname.startsWith('/api/')) {
-			// You can also use more robust routing
-			return apiRouter.handle(request);
-		}
+		// Inject Plausible for everything else
+		router.all('*', async () => {
+			const response = await fetch(request.url, request);
 
-		return new Response(
-			`Try making requests to:
-      <ul>
-      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
-      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
-			{ headers: { 'Content-Type': 'text/html' } }
-		);
-	},
+			return new HTMLRewriter()
+				.on('head', new ElementHandler())
+				.transform(response);
+		});
+
+		return router.handle(request);
+	}
 } satisfies ExportedHandler<Env>;
